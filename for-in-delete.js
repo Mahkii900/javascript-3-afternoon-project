8bfd13ce8fa64a4ebac86a7290745d8a34239766
Mahkii900 <col.graff.battleschool@gmail.com>
2019-06-28 16:07:43 -0600

    final commit

--- for-in-delete.js
+++ for-in-delete.js
@@ -40,7 +40,11 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  let str = ''
+  for (let key in obj) {
+    str += obj[key]
+  }
+  return str
 }
 
 
@@ -54,7 +58,14 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function greaterThan10(obj) {
+  for (let key in obj) {
+    if (obj[key] > 10) {
+      obj[key] = 0
+    }
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -66,8 +77,12 @@ function showValues( obj ) {
 */
 
 //Code Here
-
-
+function double(obj) {
+  for (let key in obj) {
+    obj[key] *= 2
+  }
+  return obj
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -80,7 +95,15 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function secrets(obj) {
+  let str = ''
+  for (let key in obj) {
+    if (key.startsWith('sh')) {
+      str += obj[key]
+    }
+  }
+  return str
+}
 
 
 /* 
@@ -111,7 +134,11 @@ function showValues( obj ) {
 */
 
 //Code Here
+function removePassword(obj) {
+  delete obj['password']
 
+  return obj
+}
 
 
 ////////// PROBLEM 6 //////////
@@ -130,7 +157,11 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+for (let key in deleteTheBigNumbers) {
+  if (deleteTheBigNumbers[key] > 100) {
+    delete deleteTheBigNumbers[key]
+  }
+}
 
 
 ////////// PROBLEM 7 //////////
@@ -143,7 +174,14 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+function startsWithK(obj) {
+  for (let key in obj) {
+    if (key.startsWith('k')) {
+      delete obj[key]
+    }
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 8 //////////
@@ -158,5 +196,12 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+function hiddenTreasure(obj) {
+  for (let key in obj) {
+    if (!obj[key].includes('treasure')) {
+      delete obj[key]
+    }
+  }
+  return obj
+}
 
