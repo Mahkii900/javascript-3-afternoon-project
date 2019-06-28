272c901d6eef668135090409c25b7838e0c1fd99
Mahkii900 <col.graff.battleschool@gmail.com>
2019-06-28 14:13:59 -0600

    finished nesting

--- nesting.js
+++ nesting.js
@@ -51,7 +51,17 @@ var employees = [
 */
 
 //Code Here
-
+function employeeUpdater() {
+  for (let i = 0; i < employees.length; i++) {
+    if (employees[i]['firstName'] === "Theo") {
+      employees.splice(i,1)
+    }
+    if (employees[i]['firstName'] === "Lorie") {
+      employees[i]['department'] = 'HR'
+    }
+  }
+  return employees
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -69,8 +79,16 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 //Code Here
-
-
+function removeDuplicates() {
+  let newArr = []
+  for(let i = 0; i < workplaceAccidents.length; i++) {
+    if (!newArr.includes(workplaceAccidents[i])) {
+      newArr.push(workplaceAccidents[i])
+    }
+  }
+  workplaceAccidents = newArr
+  return workplaceAccidents
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -100,7 +118,8 @@ var cat = {
 var grumpyActivity;
 var fluffy2ndFriend;
 
-
+grumpyActivity = cat['catFriends'][0]['activities'][1]
+fluffy2ndFriend = cat.catFriends[1].name
 
 ////////// PROBLEM 4 //////////
 
@@ -139,7 +158,11 @@ var myCar = {
 */
 
 //Code Here
-
+function recordCleaner() {
+  for (let i = 0; i < myCar.accidents.length; i++) {
+    myCar.accidents[i]['atFaultForAccident'] = false
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -158,5 +181,15 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 */
 
 //Code Here
-
-
+function looper() {
+  for (let i = 0; i < numsArr.length; i++) {
+    for (let j = 0; j < numsArr[i].length; j++) {
+      if (numsArr[i][j] % 2) {
+        numsArr[i][j] = 'odd'
+      } else {
+        numsArr[i][j] = 'even'
+      }
+    }
+  }
+  return numsArr
+}
